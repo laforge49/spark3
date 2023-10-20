436ee229e6dc19a1b145dea90b13f29434dc48ed
Bill La Forge <laforge49@gmail.com>
2023-10-20 14:35:41 -0400

    cactus.resolve

--- cactus_test.py
+++ cactus_test.py
@@ -1,3 +1,7 @@
-print("start test")
 import gems.cactus
-gems.cactus.printer()
+print(gems.cactus.resolve(None, "x"))
+d1 = {"a": 1}
+print(gems.cactus.resolve(d1, "x"))
+print(gems.cactus.resolve(d1, "a"))
+d2 = {"cactus.next": d1}
+print(gems.cactus.resolve(d2, "a"))
--- gems/cactus.py
+++ gems/cactus.py
@@ -1,3 +1,10 @@
 def printer():
   print("this is cactus")
   
+def resolve(leaf, key):
+  if leaf == None:
+    return None
+  value = leaf.get(key)
+  if value == None:
+    return resolve(leaf.get("cactus.next"), key)
+  return value
