1659af93bd9b5f401045aa2c8c6c28464570737b
Bill La Forge <laforge49@gmail.com>
2023-10-20 15:07:49 -0400

    flatten

--- cactus_test.py
+++ cactus_test.py
@@ -3,5 +3,10 @@ print(cactus.resolve(None, "x"))
 d1 = {"a": 1}
 print(cactus.resolve(d1, "x"))
 print(cactus.resolve(d1, "a"))
-d2 = {"cactus.next": d1}
+d2 = {"cactus.next": d1, "b": 2}
 print(cactus.resolve(d2, "a"))
+print(cactus.flatten(None))
+print(cactus.flatten(d1))
+print(cactus.flatten(d2))
+d3 = {"cactus.next": d2, "a": 23}
+print(cactus.flatten(d3))
--- gems/cactus.py
+++ gems/cactus.py
@@ -1,6 +1,6 @@
-def printer():
-  print("this is cactus")
-  
+# A cactus is a tree of dict linked by the key "cactus.next".
+
+# Find the first matching key in a stack of dict.
 def resolve(leaf, key):
   if leaf == None:
     return None
@@ -8,3 +8,14 @@ def resolve(leaf, key):
   if value == None:
     return resolve(leaf.get("cactus.next"), key)
   return value
+  
+# Create a dict with all the effective values of a stack of dict.
+def flatten (leaf):
+  if (leaf == None):
+    return {}
+  next = leaf.get("cactus.next")
+  base = flatten(next)
+  base.update(leaf)
+  base.pop("cactus.next", None)
+  return base
+  
