dc31a56013889eef07ef8c78e1d6f97a27c48480
Bill La Forge <laforge49@gmail.com>
2023-10-20 14:43:25 -0400

    use import as

--- cactus_test.py
+++ cactus_test.py
@@ -1,7 +1,7 @@
-import gems.cactus
-print(gems.cactus.resolve(None, "x"))
+import gems.cactus as cactus
+print(cactus.resolve(None, "x"))
 d1 = {"a": 1}
-print(gems.cactus.resolve(d1, "x"))
-print(gems.cactus.resolve(d1, "a"))
+print(cactus.resolve(d1, "x"))
+print(cactus.resolve(d1, "a"))
 d2 = {"cactus.next": d1}
-print(gems.cactus.resolve(d2, "a"))
+print(cactus.resolve(d2, "a"))
