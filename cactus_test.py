53819b08720cc0288bd121e827ef9e0ba9faa266
Bill La Forge <laforge49@gmail.com>
2023-10-23 13:28:15 -0400

    Update cactus_test.py

--- cactus_test.py
+++ cactus_test.py
@@ -22,22 +22,33 @@ try:
 except AttributeError:
   print("2.4 Attribute Error")
 
+print()
+print("3. TEST DATA")
+d1 = {"a": 1, "cactus.name": "root"}
+d2 = {"cactus.next": d1, "b": 2}
+d3 = {"cactus.next": d2, "a": 23, "b": None}
+print(json.dumps(d3, indent=2))
+
 """
 print(cactus.resolve(None, "x"))
-d1 = {"a": 1, "cactus.name": "root"}
 print(cactus.resolve(d1, "x"))
 print(cactus.resolve(d1, "a"))
-d2 = {"cactus.next": d1, "b": 2}
 print(cactus.resolve(d2, "a"))
-print(cactus.flatten(None))
-print(cactus.flatten(d1))
-print(cactus.flatten(d2))
-d3 = {"cactus.next": d2, "a": 23, "b": None}
 print(cactus.resolve(d3, "b"))
-print(cactus.flatten(d3))
-print(cactus.named(d3, "xyz"))
-print(cactus.named(d3, "root"))
-print(cactus.rewind(d3, 1))
-d2.update({"j": {"w": 33}})
-print(json.dumps(d3, indent=2))
 """
+
+print()
+print("5. TEST flatten")
+print("5.1", cactus.flatten(None))
+print("5.2", cactus.flatten(d1))
+print("5.3", cactus.flatten(d2))
+print("5.4", cactus.flatten(d3))
+
+print()
+print("6. TEST named")
+print("6.1", cactus.named(d3, "xyz"))
+print("6.2", cactus.named(d3, "root"))
+
+print()
+print("7. TEST rewind")
+print("7.1", cactus.rewind(d3, 1))
