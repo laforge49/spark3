70bfbd55133d20cbee4930583e189b577aba90a7
Bill La Forge <laforge49@gmail.com>
2023-10-25 13:28:23 -0400

    Update cactus_test.py

--- cactus_test.py
+++ cactus_test.py
@@ -6,21 +6,40 @@ def json_print(value):
 
 print()
 print("1. TEST _deep_munge")
+
 print("1.1", cactus._deep_munge(5, 6))
+# Output:
+# 1.1 6
+
 print("1.2", cactus._deep_munge(5, "cactus.notFound"))
+# Output:
+# 1.2 5
+
 print("1.3", cactus._deep_munge([1, 2], [2, 3]))
+# Output:
+# 1.3 [1, 2, 2, 3]
+
 print("1.4", cactus._deep_munge([], [2, 3]))
+# Output:
+# 1.4 [2, 3]
+
 print("1.5", cactus._deep_munge({"a": 1, "b": 2, "x": {"n": 22}}, {"b":3, "c":4}))
+# Output:
+# 1.5 {'b': 3, 'c': 4, 'a': 1, 'x': {'n': 22}}
 
 print()
 print("2. TEST _gets")
 print("2.1", cactus._gets(5, []))
+# Output:
 print("2.2", cactus._gets({"a": 1}, ["a"]))
+# Output:
 print("2.3", cactus._gets({"a": 1}, ["x"]))
+# Output:
 try:
   print("2.4", cactus._gets(5, ["x"]))
 except AttributeError:
   print("2.4 Attribute Error")
+# Output:
 
 print()
 print("3. TEST DATA")
@@ -28,49 +47,74 @@ d1 = {"a": 1, "cactus.name": "root"}
 d2 = {"cactus.next": d1, "b": 2, "c": [2], "d": {"q": 8, "r": 64}}
 d3 = {"cactus.next": d2, "a": 23, "b": None, "c": [3], "d": {"q": 9, "s": 99}}
 print(json.dumps(d3, indent=2))
+# Output:
 
 print()
 print("4. TEST flatten")
 print("4.1", cactus.flatten(None))
+# Output:
 print("4.2", cactus.flatten(d1))
+# Output:
 print("4.3", cactus.flatten(d2))
+# Output:
 print("4.4", cactus.flatten(d3))
+# Output:
 
 print()
 print("5. TEST resolves")
 print("5.1", cactus.resolves(None, ["x"]))
+# Output:
 print("5.2", cactus.resolves(d1, ["x"]))
+# Output:
 print("5.3", cactus.resolves(d1, ["a"]))
+# Output:
 print("5.4", cactus.resolves(d2, ["a"]))
+# Output:
 print("5.5", cactus.resolves(d3, ["a"]))
+# Output:
 print("5.6", cactus.resolves(d3, ["b"]))
+# Output:
 print("5.6", cactus.resolves(d3, ["c"]))
+# Output:
 print("5.7", cactus.resolves(d3, ["d"]))
+# Output:
 print("5.8", cactus.resolves(d3, ["d", "q"]))
+# Output:
 
 print()
 print("6. TEST named")
 print("6.1", cactus.named(d3, "xyz"))
+# Output:
 print("6.2", cactus.named(d3, "root"))
+# Output:
 
 print()
 print("7. TEST rewind")
 print("7.1", cactus.rewind(d3, 1))
+# Output:
 
 print()
 print("8. TEST puts")
+# Output:
 d4 = {}
 cactus.puts(d4, ["a"], 1)
 print("8.1", d4)
+# Output:
 cactus.puts(d4, ["b"], {"c": 2})
 print("8.2", d4)
+# Output:
 cactus.puts(d4, ["b", "d"], 39)
 print("8.3", d4)
+# Output:
 cactus.puts(d3, ["a"], 2)
 print("8.4", d3)
+# Output:
 cactus.puts(d3, ["g"], 21)
 print("8.5", d3)
+# Output:
 cactus.puts(d3, ["c"], [40])
 print("8.6", d3)
+# Output:
 cactus.puts(d3, ["d", "n"], 5)
 print("8.7", d3)
+# Output:
