d87192e570b799ef8b38225df6134c70238407fb
Bill La Forge <laforge49@gmail.com>
2023-10-25 13:35:38 -0400

    Update cactus_test.py

--- cactus_test.py
+++ cactus_test.py
@@ -54,9 +54,36 @@ print("3. TEST DATA")
 d1 = {"a": 1, "cactus.name": "root"}
 d2 = {"cactus.next": d1, "b": 2, "c": [2], "d": {"q": 8, "r": 64}}
 d3 = {"cactus.next": d2, "a": 23, "b": None, "c": [3], "d": {"q": 9, "s": 99}}
-print(json.dumps(d3, indent=2))
-# Output:
 
+print(json.dumps(d3, indent=2))
+"""
+ Output:
+{
+  "cactus.next": {
+    "cactus.next": {
+      "a": 1,
+      "cactus.name": "root"
+    },
+    "b": 2,
+    "c": [
+      2
+    ],
+    "d": {
+      "q": 8,
+      "r": 64
+    }
+  },
+  "a": 23,
+  "b": null,
+  "c": [
+    3
+  ],
+  "d": {
+    "q": 9,
+    "s": 99
+  }
+}
+"""
 print()
 print("4. TEST flatten")
 print("4.1", cactus.flatten(None))
